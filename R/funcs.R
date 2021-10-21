09138914ba90c641869b71814def9343124c3745
Aasiyah Rashan <a.rashan@me.com>
2021-10-21 16:11:57 +0100

    added tests.

--- R/funcs.R
+++ R/funcs.R
@@ -1,4 +1,5 @@
 #' Title Get Median and IQR for a particular variable. By group.
+#' Does a Mann-Whitney/K-Wallis test if there the output dataframe had a p value column.s
 #'
 #' @param data data frame containing required data
 #' @param strata variable to stratify output by. Needs to be a factor to force to ordering to work.
@@ -35,15 +36,25 @@ get_median_iqr <- function(data, strata, variable, name, output) {
 
   # Filling the first variable in with the row label.
   all <- c(paste0(name, " Median (IQR)"), total, by_strata)
-  # Renaming the variables to let the 2 data frames stack on top of each other.
-  colnames(all) <- colnames
+
+  ## Doing a Mann whitney/K-wallis test if there is a p value column.
+  if("p" %in% colnames){
+    test <- data %>%
+      summarise(test = format(round(kruskal.test(get(variable) ~ get(strata))$p.value, 5),
+                               nsmall = 5, scientific = FALSE, paired = FALSE)) %>%
+      select(test) %>%
+      t()
+    print(all)
+    all <- c(all, test)
+  }
 
   # Renaming the variables to let the 2 data frames stack on top of each other.
   output <- rbind(output, all, stringsAsFactors = FALSE)
+  colnames(output) <- colnames
   output
 }
 
-#' Title
+#' Gets count and percentage for factor variables by group. Does a chisquare test if the 'output' argument has a p value column in it.
 #'
 #' @param data
 #' @param strata
@@ -98,16 +109,30 @@ get_n_percent <- function(data, strata, variable, name, output){
   # Top row contains the variable name
   top_row <- c(paste0(name, " N(%)"), rep("", length(output) - 1))
 
+  # Doing a chi-square test if necessary
+  if("p" %in% colnames){
+    test <- data %>%
+      summarise(test = format(round(chisq.test(get(variable), get(strata))$p.value, 5),
+                              nsmall = 5, scientific = FALSE, paired = FALSE)) %>%
+      select(test) %>%
+      t()
+
+    # Overwriting the top row with the p value.
+    top_row <- c(paste0(name, " N(%)"), rep("", length(output) - 2), test)
+    # Also, the 'all' df needs an extra col for the p value
+    all['p'] <- ""
+  }
+
   all <- rbind(top_row, all)
   # Renaming the variables to let the 2 data frames stack on top of each other.
   colnames(all) <- colnames
-
   output <- rbind(output, all, stringsAsFactors = FALSE)
   output
 }
 
 #' @param data
 #' @param strata A factor variable to stratify by.
+#' @param include_tests Should it create a space for p-values?
 #'
 #' @import tidyverse
 #'
@@ -115,11 +140,18 @@ get_n_percent <- function(data, strata, variable, name, output){
 #' @export
 #'
 #' @examples
-make_output_df <- function(data, strata){
+make_output_df <- function(data, strata, include_tests = FALSE){
   # Needs to be a vector to extract the levels.
   levels <- levels(data %>%
                      pull(strata))
-  output <- data.frame(matrix(ncol = 2 + length(levels), nrow = 0))
-  colnames(output) <- c("Variable", "Total", levels)
+
+  if(include_tests){
+    output <- data.frame(matrix(ncol = 3 + length(levels), nrow = 0))
+    colnames(output) <- c("Variable", "Total", levels, "p")
+  }
+  else{
+    output <- data.frame(matrix(ncol = 2 + length(levels), nrow = 0))
+    colnames(output) <- c("Variable", "Total", levels)
+  }
   output
 }
